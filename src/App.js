3643c03a446c3cec47bbd93ba88220ec4d3290f8
xNoJustice <harunmimarsinan@gmail.com>
2021-02-28 19:42:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { MainProvider } from "./mainContext";
+import { UsersProvider } from "./usersContext";
+import { SocketProvider } from "./socketContext";
+import Login from "./components/Login";
+import Chat from "./components/Chat";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <MainProvider>
+    <UsersProvider>
+      <SocketProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route path="/chat" component={Chat} />
+          </Switch>
+        </Router>
+      </SocketProvider>
+    </UsersProvider>
+  </MainProvider>
+);
 
 export default App;
